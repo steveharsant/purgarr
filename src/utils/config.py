ee91aa442db4d247f31f6c31ea9c27d27a901c72
steveharsant <stevenharsant@gmail.com>
2025-06-03 15:20:07 +1000

    config file tidy up

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,14 +3,18 @@ import os
 
 load_dotenv()
 
-
 def ensure_http(url):
     return url if url.startswith(("http://", "https://")) else "http://" + url
 
+# Logging & WebUI
+log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+log_output = os.getenv("LOG_OUTPUT", "all").lower()
+web_host = os.getenv("WEB_HOST", "http://localhost").lower()
+web_port = int(os.getenv("WEB_PORT", 9891))
+log_lines = int(os.getenv("LOG_LINES", 100))
+log_refresh_interval = int(os.getenv("LOG_REFRESH_INTERVAL", 5))
 
-# Purgarr Behaviour
-block_stalled_torrents = os.getenv("BLOCK_STALLED_TORRENTS", "True").lower() == "true"
-delete_files = os.getenv("DELETE_FILES", "True").lower() == "true"
+# Extension Purgarr
 purge_extensions = os.getenv("PURGE_EXTENSIONS", "False").lower() == "true"
 purge_extensions_interval = int(os.getenv("PURGE_EXTENSIONS_INTERVAL", 60))
 blocked_extensions = (
@@ -21,19 +25,21 @@ blocked_extensions = (
     .lower()
     .split(",")
 )
-log_level = os.getenv("LOG_LEVEL", "INFO").upper()
-log_output = os.getenv("LOG_OUTPUT", "all").lower()
-purge_imported = os.getenv("PURGE_IMPORTED", "True").lower() == "true"
-purge_imported_interval = int(os.getenv("PURGE_IMPORTED_INTERVAL", 600))
+
+# Stalled Purgarr
 purge_stalled = os.getenv("PURGE_STALLED", "True").lower() == "true"
 purge_stalled_interval = int(os.getenv("PURGE_STALLED_INTERVAL", 300))
-retry_search = os.getenv("RETRY_SEARCH", "True").lower() == "true"
-web_host = os.getenv("WEB_HOST", "http://localhost").lower()
-web_port = int(os.getenv("WEB_PORT", 9891))
+block_stalled_torrents = os.getenv("BLOCK_STALLED_TORRENTS", "True").lower() == "true"
 
-# Torrents
+# Purgarr Behaviour
+purge_imported = os.getenv("PURGE_IMPORTED", "True").lower() == "true"
+purge_imported_interval = int(os.getenv("PURGE_IMPORTED_INTERVAL", 600))
+retry_search = os.getenv("RETRY_SEARCH", "True").lower() == "true"
+
+# Torrent Client
 torrent_client = os.getenv("TORRENT_CLIENT", "qbittorrent")
 torrent_age = os.getenv("TORRENT_AGE", 5)
+delete_files = os.getenv("DELETE_FILES", "True").lower() == "true"
 
 # qBittorrent
 qbit_url = ensure_http(os.getenv("QBIT_URL", "localhost:8080")) + "/api/v2"
@@ -53,11 +59,7 @@ radarr_url = ensure_http(os.getenv("RADARR_URL", "localhost:7878")) + "/api/v3"
 radarr_api_key = os.getenv("RADARR_API_KEY")
 radarr_imported_label = os.getenv("RADARR_IMPORTED_LABEL", "radarr-imported")
 
-# webUI
-log_lines = int(os.getenv("LOG_LINES", 100))
-log_refresh_interval = int(os.getenv("LOG_REFRESH_INTERVAL", 5))
-
-# Other
+# Parsed Variables
 import_labels = os.getenv(
     "IMPORT_LABELS", f"{sonarr_imported_label}, {radarr_imported_label}"
 ).split(",")
